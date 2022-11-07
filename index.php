f4af9e93e7461236d72d682d3c3d7bca6ede4413
Erik Mazzuco <erikmazzucomatheus789@gmail.com>
2022-11-07 17:25:07 -0300

    mano

--- index.php
+++ index.php
@@ -1,47 +1,120 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
-<html lang="pt-br">
+
+<html lang="pt">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Ideal Music</title>
+    <link rel="stylesheet" href="menu.css">
 </head>
-<body class="LOGIN">
-
-<header>
-<div class="cointer-logo">
-    <div class="logo-imagem"></div>
-    <div class="logo-texto">
-
-    <?php
-
-
-if(isset($_SESSION['id'])) {
-    
-        echo $_SESSION['nome']; 
-}else{
-    
-    $_SESSION['msg'] = '<a href="LOGIN/index.php">Entrar</a>';
-    echo $_SESSION['msg']; 
-}
-    ?>
-
+<body>
+    <header>
+        <div class="logo">
+            <img src="C:\Users\educa\OneDrive\Área de Trabalho\tcc_info\Ideal Music (2).png" alt="logo">
+        </div> 
+        <div class="menu">
+            <ul>
+                <li><a href="#inicio">Início</a></li>
+                <li><a href="#destaques">Destaques</a></li>
+                <li><a href="#produtos">Produtos</a></li>
+                <li><a href="#sobre">Sobre</a></li>
+                <li><a href="#contato">Contato</a></li>
+                <li><a href="#login">Login</a></li>
+            </ul>
         </div>
-</div>
+        <div class="btn"><ion-icon name="grid-sharp"></ion-icon></div>
+    </header>
+</br></br></br>
+    <main>
 
-<div class="menu">
-    <ul>
-        <li><a href="#">sobre</a></li>
-        <li><a href="#">contato</a></li>
-        <li><a href="#">PRODOTOS</a></li>
-        <li><a href="LOGIN/logout.php">Sair</a></li>
-    </ul>
-</header>
+        <!--    DESTAQUES   -->
+        <section id="destaques">
+            <div class="container">
+                <div class="title">
+                    <div class="line">
+                        <h1>Destaques</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
 
-    
+         <!--    INICIO    -->
+         <section id="inicio">
+            <div class="container">
+                <div class="content">
+                    <div class="call">
+                        <h1>IDEAL MUSIC</h1>
+                        <p>A loja IDEAL para montar a sua banda!</p>
+                    </div>    
+                </div>    
+            </div>
+        </section>
+
+
+         <!--      PRODUCTS     -->
+
+         <section id="products">
+            <div class="container">
+                <div class="title">
+                    <div class="line"></div>
+                    <h1>Produtos</h1>
+                </div>
+                <div class="card owl-carousel">
+                    <div class="box">
+                        <img src="C:\Users\educa\OneDrive\Área de Trabalho\tcc_info\guitarra1.webp" alt="guitarra1">
+                        <h1>GUITARRA</h1>
+                        <p>$ 1.000</p>
+                        <div class="icon">
+                            <ion-icon name="bag-outline"></ion-icon>
+                        </div>
+
+                        <div class="box">
+                            <img src="C:\Users\educa\OneDrive\Área de Trabalho\tcc_info\violao1.webp" alt="violao1">
+                            <h1>VIOLÃO</h1>
+                            <p>$ 600</p>
+                            <div class="icon">
+                                <ion-icon name="bag-outline"></ion-icon>
+                            </div>
+                        </div>
+
+                        <div class="box">
+                            <img src="C:\Users\educa\OneDrive\Área de Trabalho\tcc_info\baixo1.webp" alt="baixo1">
+                            <h1>BAIXO</h1>
+                            <p>$ 800</p>
+                            <div class="icon">
+                                <ion-icon name="bag-outline"></ion-icon>
+                            </div>
+                        </div>
+                        <div class="box">
+                            <img src="C:\Users\educa\OneDrive\Área de Trabalho\tcc_info\cajon1.webp" alt="cajon1">
+                            <h1>CAJÓN</h1>
+                            <p>$ 500</p>
+                            <div class="icon">
+                                <ion-icon name="bag-outline"></ion-icon>
+                            </div>
+                        </div>
+                        <div class="box">
+                            <img src="C:\Users\educa\OneDrive\Área de Trabalho\tcc_info\teclado1.webp" alt="teclado1">
+                            <h1>TECLADO</h1>
+                            <p>$ 1.200</p>
+                            <div class="icon">
+                                <ion-icon name="bag-outline"></ion-icon>
+                            </div>
+                        </div>
+                        <div class="box">
+                            <img src="C:\Users\educa\OneDrive\Área de Trabalho\tcc_info\acordeon4.webp" alt="acordeon4">
+                            <h1>ACORDEON</h1>
+                            <p>$ 5.500</p>
+                            <div class="icon">
+                                <ion-icon name="bag-outline"></ion-icon>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+            </section>
+    </main>
+
+
 </body>
 </html>
