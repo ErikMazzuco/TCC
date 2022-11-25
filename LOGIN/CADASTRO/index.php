8c9aab7ff65397f3f75dba6144b179d9929eb853
Erik Mazzuco <erikmazzucomatheus789@gmail.com>
2022-11-25 00:50:58 -0300

    oxi

--- LOGIN/CADASTRO/index.php
+++ LOGIN/CADASTRO/index.php
@@ -9,12 +9,10 @@ session_start();
 <meta charset="utf-8">
 <link rel="stylesheet" href="style.css">
 <title> Cadastrar: </title>
-
 </head>
 <body>
 
-
-<?php
+<!--
 
 //se existir a variavel imprime a variavel global
 
@@ -23,49 +21,37 @@ echo $_SESSION['msg'];
 unset($_SESSION['msg']); //destroi a variavel global
 }
 
-?>
+-->
 <div class="main">
     <div class="center">
 
-<form  method="post" class="card-cadastro" action="processa.php">
-<h1>Criar Conta</h1>
+        <form  method="post" class="card-cadastro" action="processa.php">
+            <h1>Criar Conta</h1>
 
-<div class="textfield"> 
-<label>Nome: </label>
-<input type="text" id="nome" name="nome" placeholder="Digite seu nome" required><br><br>
+            <div class="textfield"> 
+            <label>Nome: </label>
+            <input type="text" id="nome" name="nome" placeholder="Digite seu nome" required><br><br>
+            </div>
+
+            <div class="textfield"> 
+            <label>E-mail: </label>
+            <input type="email" id="email" name="email" placeholder="Digite o seu e-mail" required><br><br>
+            </div>
+
+            <div class="textfield"> 
+            <label>SENHA: </label>
+            <input type="password" id="senha" name="senha" placeholder="Digite uma senha" required><br><br>
+            </div>
+
+            <input type="submit" class="btn-cadastro" value="Criar Minha Conta">
+
+            <p>Já possui conta? <a href="../index.php"><font color="gray">Entre</a></p></font>
+
+            <center>
+            <p>Ao criar uma conta você concorda com os
+            <a href="http://tcc-2022.herokuapp.com/PRIVACY/"><font color="gray">Termos e Politica de Privacidade</a>
+        </form>
+    </div>
 </div>
-
-<div class="textfield"> 
-<label>E-mail: </label>
-<input type="email" id="email" name="email" placeholder="Digite o seu e-mail" required><br><br>
-</div>
-
-
-<div class="textfield"> 
-<label>SENHA: </label>
-<input type="password" id="senha" name="senha" placeholder="Digite uma senha" required><br><br>
-</div>
-
-<input type="submit" class="btn-cadastro" value="Criar Minha Conta">
-
-<p>Já possui conta? <a href="../index.php"><font color="gray">Entre</a></p></font>
-
-<center>
-<p>Ao criar uma conta você concorda com os
-<a href="http://tcc-2022.herokuapp.com/PRIVACY/"><font color="gray">Termos e Politica de Privacidade</a>
-
-
-
-
-
-
-
-</form>
-
-
-</div>
-</div>
-
 </body>
-
 </html>
